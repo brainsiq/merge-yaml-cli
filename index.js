9b6a5f1e105ab4741522bfe9c143f5957c588c52
Chris Impey <chris.impey@cloudmargin.com>
2019-05-20 14:55:06 +0100

    Replace vulnerable merge-yaml dep with few lines of custom code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
+const fs = require('fs')
 const glob = require('glob')
-const mergeYaml = require('merge-yaml')
+const merge = require('lodash.merge')
 const jsYaml = require('js-yaml')
 const EventEmitter = require('events')
 
@@ -11,10 +12,14 @@ class YamlMerger extends EventEmitter {
 
     this.emit('files', files)
 
-    const mergeResult = mergeYaml(files)
+    const mergeResult = files.reduce((merged, file) => {
+      const yaml = jsYaml.safeLoad(fs.readFileSync(file, 'utf8'))
+
+      return merged ? merge(merged, yaml) : yaml
+    }, null)
 
     return jsYaml.safeDump(mergeResult)
   }
-};
+}
 
 module.exports = new YamlMerger()
