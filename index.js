d239c169463321916465e3d41d68255b26c1fe70
Chris Impey <chris.impey@cloudmargin.com>
2019-05-20 14:57:38 +0100

    Update standard dependency

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const EventEmitter = require('events')
 
 class YamlMerger extends EventEmitter {
   merge (sources) {
-    const files = [].concat.apply([], sources.map(g => glob.sync(g, {nodir: true})))
+    const files = [].concat.apply([], sources.map(g => glob.sync(g, { nodir: true })))
 
     this.emit('files', files)
 
