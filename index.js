3a09068b8d4399033513395d94105f09316b5dd9
Vuk Stankovic <vuks89@gmail.com>
2021-05-17 13:04:39 +0200

    fix: update dependencies and node version

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const EventEmitter = require('events')
 
 class YamlMerger extends EventEmitter {
   merge (sources) {
-    const files = [].concat.apply([], sources.map(g => glob.sync(g, {nodir: true})))
+    const files = [].concat.apply([], sources.map(g => glob.sync(g, { nodir: true })))
 
     this.emit('files', files)
 
     const mergeResult = mergeYaml(files)
 
-    return jsYaml.safeDump(mergeResult)
+    return jsYaml.dump(mergeResult)
   }
 };
 
